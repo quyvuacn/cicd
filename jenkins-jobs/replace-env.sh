e53e029aa0b81bdba973f846e50e95ee127db920
Vũ Quý <vuvietquyacn@gmail.com>
2024-10-13 16:34:58 +0700

    update

--- jenkins-jobs/replace-env.sh
+++ jenkins-jobs/replace-env.sh
@@ -1,18 +1,18 @@
-    #!/bin/bash
-    current_color=$1
-    target_file=$2
-    tem_path="./conf.d.tem/$target_file"
-    docker_conf_path="etc/nginx/conf.d/$target_file"
+#!/bin/bash
+current_color=$1
+target_file=$2
+tem_path="./conf.d.tem/$target_file"
+docker_conf_path="etc/nginx/conf.d/$target_file"
 
-    if [ "$current_color" == "blue" ]; then
-        switch_color="green"
-    else
-        switch_color="blue"
-    fi
+if [ "$current_color" == "blue" ]; then
+    switch_color="green"
+else
+    switch_color="blue"
+fi
 
-    conf=$(sed "s|\$current_color|$switch_color|g" "$tem_path")
+conf=$(sed "s|\$current_color|$switch_color|g" "$tem_path")
 
-    container_id=$(docker ps -qf "name=nginx")
-    echo "$conf" | docker exec -i "$container_id" bash -c "cat > $docker_conf_path"
+container_id=$(docker ps -qf "name=nginx")
+echo "$conf" | docker exec -i "$container_id" bash -c "cat > $docker_conf_path"
 
-    docker exec -i "$container_id" nginx -s reload
+docker exec -i "$container_id" nginx -s reload
